b7208badd6461f0064d683eca4e1c319fcc3ff39
GuoShuai <1561726052@qq.com>
2017-09-17 21:17:31 +0800

    2017.9.17 21:17

--- SiteReservation/forms.py
+++ SiteReservation/forms.py
@@ -7,7 +7,14 @@
 # Filename: forms.py
 # Description:
 from django import forms
-
+from django.forms import ModelForm
+from django.contrib.admin import widgets
+from django.forms.extras.widgets import SelectDateWidget
+from .models import Reservation
+from const.models import CaptchaField
+from SiteReservation.models import Reservation
+from SiteReservation import RESERVATION_APPROVED
+from django.db.models import Q
 
 class DateForm(forms.Form):
     date = forms.DateField(
@@ -15,3 +22,40 @@ class DateForm(forms.Form):
         widget=forms.TextInput(attrs={
             'readonly': 'true',
             'class': 'form-control'}))
+
+class ReservationAddForm(ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['site', 'workshop', 'title',
+                  'activity_time_from', 'activity_time_to', 'comment']
+
+    def clean(self):
+        cleaned_data = super(ReservationAddForm, self).clean()
+        errors = {}
+        t1 = cleaned_data.get('activity_time_from')
+        t2 = cleaned_data.get('activity_time_to')
+        site_now = cleaned_data.get('site')
+
+        if t1.hour < 8 or t1.hour >22:
+            errors['activity_time_from'] = ['活动应在早8点至晚10点间']
+
+        if t2.hour < 8 or t2.hour >22:
+            errors['activity_time_to'] = ['活动时间应在早8点至晚10点间']
+
+        if t2 <= t1:
+            if 'activity_time_from' in errors:
+                errors['activity_time_from'].append('活动结束时间应晚于开始时间')
+            else:
+                errors['activity_time_from'] = ['活动结束时间应晚于开始时间']
+
+
+        qset = Reservation.objects.filter(status=RESERVATION_APPROVED)
+        qset = qset.filter(Q(site=site_now))
+        qset = qset.filter((Q(activity_time_to__gt=t2)&Q(activity_time_from__lt=t1))|(Q(activity_time_from__lt=t2)&Q(activity_time_to__gt=t1)))
+        qnum = qset.count()
+        if qnum != 0:
+            errors['activity_time_to'] = ['该时间段内已存在预约']
+        if errors:
+            raise forms.ValidationError(errors)
+
+        return cleaned_data
